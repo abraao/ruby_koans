779dd76efc1a2d029e6ca7531f4645c266cf5018
Abraão L. N. Lourenço <gitcommit@abraao.cat>
2022-05-27 15:05:49 +0200

    Completed koan on dice project

--- koans/about_dice_project.rb
+++ koans/about_dice_project.rb
@@ -6,6 +6,18 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #   code ...
 # end
 
+class DiceSet
+  attr_reader :values
+
+  def initialize
+    @values = []
+  end
+
+  def roll(num_die)
+    @values = num_die.times.map {|x| rand(1..6)}
+  end
+end
+
 class AboutDiceProject < Neo::Koan
   def test_can_create_a_dice_set
     dice = DiceSet.new
@@ -48,6 +60,8 @@ class AboutDiceProject < Neo::Koan
     # If the rolls are random, then it is possible (although not
     # likely) that two consecutive rolls are equal.  What would be a
     # better way to test this?
+    #
+    # We can test using mocks and verifying expectations that a roll actually does something.
   end
 
   def test_you_can_roll_different_numbers_of_dice
